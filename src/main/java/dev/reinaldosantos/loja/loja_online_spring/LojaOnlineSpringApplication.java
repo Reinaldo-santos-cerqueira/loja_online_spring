16166b11830bfda71e89374837097e53248181f0
Isaac Batista <ibatista@HKORGANIZE.local>
2024-06-11 15:42:00 -0300

    chore: init project

---
+++ src/main/java/dev/reinaldosantos/loja/loja_online_spring/LojaOnlineSpringApplication.java
@@ -0,0 +1,13 @@
+package dev.reinaldosantos.loja.loja_online_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaOnlineSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaOnlineSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/reinaldosantos/loja/loja_online_spring/LojaOnlineSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.reinaldosantos.loja.loja_online_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaOnlineSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
