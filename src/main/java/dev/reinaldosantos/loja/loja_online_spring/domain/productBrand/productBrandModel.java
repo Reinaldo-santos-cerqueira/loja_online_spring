c11500eccc7f2b3a736a94ae2354fec5c199548f
Isaac Batista <ibatista@HKORGANIZE.local>
2024-06-17 14:01:16 -0300

    feat: added hashcode and equals to class productBrand

--- src/main/java/dev/reinaldosantos/loja/loja_online_spring/domain/productBrand/productBrandModel.java
+++ src/main/java/dev/reinaldosantos/loja/loja_online_spring/domain/productBrand/productBrandModel.java
@@ -2,6 +2,7 @@ package dev.reinaldosantos.loja.loja_online_spring.domain.productBrand;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -12,12 +13,13 @@ import jakarta.persistence.Table;
 @Entity
 @Table(name = "product_brand")
 @SequenceGenerator(name = "sequence_product_brand",sequenceName = "sequence_product_brand",allocationSize = 1,initialValue = 1)
-public class productBrandModel implements Serializable {
+public class ProductBrandModel implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequence_product_brand")
     private Long id;
+    @Column(name = "description", nullable = false)
     private String description;
     public Long getId() {
         return id;
@@ -31,7 +33,30 @@ public class productBrandModel implements Serializable {
     public void setDescription(String description) {
         this.description = description;
     }
-    public productBrandModel() {
+    public ProductBrandModel() {
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        ProductBrandModel other = (ProductBrandModel) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
 }
