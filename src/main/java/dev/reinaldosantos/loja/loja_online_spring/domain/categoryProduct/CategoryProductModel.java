f97bb89630670d85471a369a0b23adf70d5dc09a
Isaac Batista <ibatista@HKORGANIZE.local>
2024-06-17 14:01:54 -0300

    feat: create class CategoryProduct

---
+++ src/main/java/dev/reinaldosantos/loja/loja_online_spring/domain/categoryProduct/CategoryProductModel.java
@@ -0,0 +1,59 @@
+package dev.reinaldosantos.loja.loja_online_spring.domain.categoryProduct;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "category_product")
+@SequenceGenerator(name = "seq_category_product", sequenceName = "seq_category_product",allocationSize = 1, initialValue = 1)
+public class CategoryProductModel implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "seq_category_product")
+    private Long id;
+    private String description;
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        CategoryProductModel other = (CategoryProductModel) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+}
