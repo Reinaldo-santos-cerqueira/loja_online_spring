d7723cc60981c36a3ef14099732c2299aa5a0dd4
reinaldo-santosBa <reinaldodevcontato@gmail.com>
2024-06-18 22:42:24 -0300

    feat: created AccessModel for access

---
+++ src/main/java/dev/reinaldosantos/loja/loja_online_spring/domain/access/AccessModel.java
@@ -0,0 +1,74 @@
+package dev.reinaldosantos.loja.loja_online_spring.domain.access;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="access")
+@SequenceGenerator(name = "seq_access",sequenceName = "seq_access",allocationSize = 1,initialValue = 1)
+public class AccessModel implements GrantedAuthority{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "seq_access")
+    private Long id;
+
+    @Column(nullable = false)
+    private String description;
+    
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        AccessModel other = (AccessModel) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String getAuthority() {
+        return this.description;
+    }
+
+    public AccessModel() {
+    }
+    
+}
